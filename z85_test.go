d0cdc62e76b724147bf05618a189712390b72e6d
Daniel Jones <tortxof@gmail.com>
2026-01-12 00:53:21 -0500

    Add streaming encoder and decoder

--- z85_test.go
+++ z85_test.go
@@ -37,37 +37,37 @@ func TestZ85EncodeChunk(t *testing.T) {
 	}
 }
 
+var encodeTests = []struct {
+	name  string
+	input []byte
+	want  string
+}{
+	{
+		name:  "8 bytes",
+		input: []byte{0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B},
+		want:  "HelloWorld",
+	},
+	{
+		name:  "text with padding",
+		input: []byte("Hello world!!"),
+		want:  "nm=QNzY<mxA+]nfaP",
+	},
+	{
+		name:  "empty input",
+		input: []byte{},
+		want:  "",
+	},
+	{
+		name:  "single byte",
+		input: []byte{0x42},
+		want:  "li",
+	},
+}
+
 func TestZ85Encode(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name  string
-		input []byte
-		want  string
-	}{
-		{
-			name:  "8 bytes",
-			input: []byte{0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B},
-			want:  "HelloWorld",
-		},
-		{
-			name:  "text with padding",
-			input: []byte("Hello world!!"),
-			want:  "nm=QNzY<mxA+]nfaP",
-		},
-		{
-			name:  "empty input",
-			input: []byte{},
-			want:  "",
-		},
-		{
-			name:  "single byte",
-			input: []byte{0x42},
-			want:  "li",
-		},
-	}
-
-	for _, tt := range tests {
+	for _, tt := range encodeTests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Z85Encode(tt.input)
 			if string(got) != tt.want {
@@ -77,40 +77,40 @@ func TestZ85Encode(t *testing.T) {
 	}
 }
 
+var roundTripTests = []struct {
+	name  string
+	input []byte
+}{
+	{
+		name:  "8 bytes",
+		input: []byte{0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B},
+	},
+	{
+		name:  "text",
+		input: []byte("Hello world!!"),
+	},
+	{
+		name:  "empty",
+		input: []byte{},
+	},
+	{
+		name:  "single byte",
+		input: []byte{0x42},
+	},
+	{
+		name:  "all zeros",
+		input: []byte{0x00, 0x00, 0x00, 0x00},
+	},
+	{
+		name:  "all ones",
+		input: []byte{0xFF, 0xFF, 0xFF, 0xFF},
+	},
+}
+
 func TestZ85RoundTrip(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name  string
-		input []byte
-	}{
-		{
-			name:  "8 bytes",
-			input: []byte{0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B},
-		},
-		{
-			name:  "text",
-			input: []byte("Hello world!!"),
-		},
-		{
-			name:  "empty",
-			input: []byte{},
-		},
-		{
-			name:  "single byte",
-			input: []byte{0x42},
-		},
-		{
-			name:  "all zeros",
-			input: []byte{0x00, 0x00, 0x00, 0x00},
-		},
-		{
-			name:  "all ones",
-			input: []byte{0xFF, 0xFF, 0xFF, 0xFF},
-		},
-	}
-
-	for _, tt := range tests {
+	for _, tt := range roundTripTests {
 		t.Run(tt.name, func(t *testing.T) {
 			encoded := Z85Encode(tt.input)
 			decoded := Z85Decode(encoded)
@@ -168,6 +168,54 @@ func BenchmarkZ85Decode(b *testing.B) {
 
 			for i := 0; i < b.N; i++ {
 				Z85Decode(encoded)
+			}
+		})
+	}
+}
+
+func TestStreamEncode(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range encodeTests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			encoder := NewEncoder(&buf)
+			encoder.Write(tt.input)
+			err := encoder.Close()
+			if err != nil {
+				t.Fatalf("Close() returned an unexpected error: %v", err)
+			}
+			got := buf.String()
+			if string(got) != tt.want {
+				t.Errorf("Encoder(%v) = %q, want %q", tt.input, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStreamRoundTrip(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range roundTripTests {
+		t.Run(tt.name, func(t *testing.T) {
+			var encBuf bytes.Buffer
+			encoder := NewEncoder(&encBuf)
+			encoder.Write(tt.input)
+			err := encoder.Close()
+			if err != nil {
+				t.Fatalf("Close() returned an unexpected error: %v", err)
+			}
+			encoded := encBuf.Bytes()
+			var decBuf bytes.Buffer
+			decoder := NewDecoder(&decBuf)
+			decoder.Write(encoded)
+			err = decoder.Close()
+			if err != nil {
+				t.Fatalf("Close() returned an unexpected error: %v", err)
+			}
+			decoded := decBuf.Bytes()
+			if !bytes.Equal(decoded, tt.input) {
+				t.Errorf("Round trip failed: input %v, encoded %q, decoded %v", tt.input, encoded, decoded)
 			}
 		})
 	}
